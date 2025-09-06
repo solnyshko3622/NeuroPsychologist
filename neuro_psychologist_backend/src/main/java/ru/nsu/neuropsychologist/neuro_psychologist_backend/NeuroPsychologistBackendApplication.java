a159bcbf8053c7b966d5209e138003b05483a599
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2025-09-06 15:33:39 +0700

    NEUROPSYCHOLOGIST-1: init backend

---
+++ neuro_psychologist_backend/src/main/java/ru/nsu/neuropsychologist/neuro_psychologist_backend/NeuroPsychologistBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.neuropsychologist.neuro_psychologist_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroPsychologistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroPsychologistBackendApplication.class, args);
+    }
+
+}
